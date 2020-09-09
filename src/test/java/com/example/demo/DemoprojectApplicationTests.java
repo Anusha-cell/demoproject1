c04452313ae9e2b40edbc0e472648d1b6c8da29e
hp <hp@LAPTOP-IUMUBCFO>
2020-09-10 00:25:49 +0530

    commit

---
+++ src/test/java/com/example/demo/DemoprojectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	public DemoprojectApplicationTests()
+	{}
+}
